8043c0f38d3e86bf5d0edcfcb908ef568f4255ee
moonyango <mosesadrian825@gmail.com>
2025-04-02 16:32:14 +0300

    feat: groceries

---
+++ groceries/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn insert(vec: &mut Vec<String>, val: String) {
+    v.push(val)
+}
+
+pub fn at_index(slice: &[String], index: usize) -> &str {
+    v.index
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
