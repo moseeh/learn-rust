a3373cecd294c11ba8afbdd2bce82a89ddc8335a
moonyango <mosesadrian825@gmail.com>
2025-04-02 17:28:32 +0300

    fix : logic

--- find_factorial/src/lib.rs
+++ find_factorial/src/lib.rs
@@ -1,18 +1,20 @@
 pub fn factorial(num: u64) -> u64 {
     match num {
         0 | 1 => 1,
-    };
-    let mut n = 1;
-    loop {
-        if num == 1 {
-            break
+        _ => {
+            let mut n = 1;
+            let mut current = num;
+            loop {
+                if current == 1 {
+                    break;
+                }
+                n = n * current;
+                current = current - 1;
+            }
+            n
         }
-        n = n * num;
-        num = num -1
     }
-    n
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
