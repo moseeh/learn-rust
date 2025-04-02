e3eca4f57e21b3c57c69e247c1640e2e2053f152
moonyango <mosesadrian825@gmail.com>
2025-04-02 18:59:50 +0300

    feat: doubtful

---
+++ doubtful/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn doubtful(s: &str ) {
+    
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
