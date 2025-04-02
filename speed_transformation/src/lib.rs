8526614ac6874b481e6514ee2f52ea8c287eb5bb
moonyango <mosesadrian825@gmail.com>
2025-04-02 03:35:56 +0300

    feat: km per hr to m per s

---
+++ speed_transformation/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn km_per_hour_to_meters_per_second(km_h: f64) -> f64 {
+    km_h * 5.0/18.0
+
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
