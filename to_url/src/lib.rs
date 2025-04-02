e6f0367779b5902844d347647f46c9110844b2e1
moonyango <mosesadrian825@gmail.com>
2025-04-03 00:42:35 +0300

    replace string

---
+++ to_url/src/lib.rs
@@ -0,0 +1,13 @@
+pub fn to_url(s: &str) -> String {
+    s.replace(" ", "%20")
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
