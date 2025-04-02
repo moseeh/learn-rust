3a67804acf6dd562755a1543ea63ccf332f16470
moonyango <mosesadrian825@gmail.com>
2025-04-02 03:23:51 +0300

    feat: fahrenheit conversions

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,9 +1,9 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    (f - 32) * (5 / 9)
+    (f - 32.0) * (5.0 / 9.0)
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
-    (c * 9 / 5) + 32
+    (c * 9.0 / 5.0) + 32.0
 }
 #[cfg(test)]
 mod tests {
