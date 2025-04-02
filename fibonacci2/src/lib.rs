a5f6288f703b922c490428e852eba20f11388374
moonyango <mosesadrian825@gmail.com>
2025-04-02 17:34:28 +0300

    feat: fibonacci

---
+++ fibonacci2/src/lib.rs
@@ -0,0 +1,26 @@
+pub fn fibonacci(n: u32) -> u32 {
+    match n {
+        0 => 0,
+        1 => 1,
+        _ => {
+            let mut a = 0;
+            let mut b = 1;
+            for _ in 2..=n {
+                let temp = a + b;
+                a = b;
+                b = temp;
+            }
+            b
+        }
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
