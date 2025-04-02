9a36218684b9d57d57ef77bbf6f0789df6f20335
moonyango <mosesadrian825@gmail.com>
2025-04-02 12:08:13 +0300

    feat:str length

---
+++ borrow/src/lib.rs
@@ -0,0 +1,13 @@
+pub fn str_len(s: &str ) -> usize {
+    s.chars().count()
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
