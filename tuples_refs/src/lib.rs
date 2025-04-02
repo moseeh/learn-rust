4fd8a6ad5a3962d786e07ba1642e5914ab28c4df
moonyango <mosesadrian825@gmail.com>
2025-04-03 00:34:07 +0300

    tuple refs

---
+++ tuples_refs/src/lib.rs
@@ -0,0 +1,24 @@
+pub struct Student(pub u32, pub String, pub String);
+
+pub fn id(student: &Student) -> u32 {
+    student.0
+}
+
+pub fn first_name(student: &Student) -> &str {
+    &student.1
+}
+
+pub fn last_name(student: &Student) -> &str {
+    &student.2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
