36166bfc1a46ab6b7cffa6d3bf805fee15abeeb5
moonyango <mosesadrian825@gmail.com>
2025-04-02 04:03:59 +0300

    looping

---
+++ looping/src/main.rs
@@ -0,0 +1,21 @@
+use std::io;
+
+fn main() {
+    let mut i = 0;
+    let answer = "The letter e";
+    let mut guess = String::new();
+    loop {
+        println!(
+            "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?"
+        );
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read the line");
+        if guess == answer {
+            println!("Number of trials: {}", i);
+            break
+        } else {
+          i= i +1;
+        }
+    }
+}
