e8798de6890f90bceae686a3b4be588f95aa3eb5
moonyango <mosesadrian825@gmail.com>
2025-04-06 01:12:44 +0300

    hashmaps

---
+++ simple_hash/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn word_frequency_counter(words: Vec<&str>) -> HashMap<&str, usize> {
+    let map = HashMap::new();
+    for word in words {
+        map.entry(word).or_insert(0) += 1;
+    }
+    map
+}
+
+pub fn nb_distinct_words(frequency_count: &HashMap<&str, usize>) -> usize {
+    frequency_count.len()
+}
