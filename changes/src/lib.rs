8ebaa666832d05080d8b4195bfbd37a27e19e75a
moonyango <mosesadrian825@gmail.com>
2025-04-05 22:29:43 +0300

    change lights

---
+++ changes/src/lib.rs
@@ -0,0 +1,24 @@
+#[derive(Debug, Eq, PartialEq, Clone)]
+
+pub struct Light {
+    pub alias: String,
+    pub brightness: u8,
+}
+
+impl Light {
+    pub fn new(alias: &str) -> Self {
+        Self{
+            alias: alias,
+            brightness: 0,
+        }
+    }
+}
+
+pub fn change_brightness(lights: &mut [Light], alias: &str, value: u8) {
+    for l in lights {
+        if l.alias == alias {
+            l.brightness = value
+        }
+    }
+
+}
