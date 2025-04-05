130925a7dbdc00812a658a8e5ffbe3d77351a1f3
moonyango <mosesadrian825@gmail.com>
2025-04-06 01:24:03 +0300

    lifetime parameter

--- simple_hash/src/lib.rs
+++ simple_hash/src/lib.rs
@@ -1,8 +1,8 @@
 use std::collections::HashMap;
 
-pub fn word_frequency_counter<'a>(words: Vec<&'a str>) -> HashMap<&'a str, usize> {
+pub fn word_frequency_counter<'a>(words: &'a [&'a str]) -> HashMap<&'a str, usize> {
     let mut map = HashMap::new();
-    for word in words {
+    for &word in words {
         *map.entry(word).or_insert(0) += 1;
     }
     map
