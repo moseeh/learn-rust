9a73d60e494df1ea83cad1d1d14e2f91c094e414
moonyango <mosesadrian825@gmail.com>
2025-04-06 01:17:26 +0300

    references

--- simple_hash/src/lib.rs
+++ simple_hash/src/lib.rs
@@ -2,8 +2,8 @@ use std::collections::HashMap;
 
 pub fn word_frequency_counter(words: Vec<&str>) -> HashMap<&str, usize> {
     let map = HashMap::new();
-    for word in words {
-        map.entry(word).or_insert(0) += 1;
+    for &word in words {
+        *map.entry(word).or_insert(0) += 1;
     }
     map
 }
