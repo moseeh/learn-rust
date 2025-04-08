b5d43a2d6e2d58f8c9308a316eb312c5fd88ef8a
moonyango <mosesadrian825@gmail.com>
2025-04-08 18:20:04 +0300

    feat: to string

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -8,10 +8,10 @@ pub enum Security {
 
 pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String {
     match security_level {
-        Security:: Unknown => server.unwrap(),
-        Security:: Message => server.expect("ERROR: program stops"),
-        Security:: Warning => server.expect("WARNING: check the server"),
-        Security:: NotFound => server.expect("Not found: [{}]", server.ERROR),
-        Security:: UnexpectedUrl => server.unwrap()
+        Security:: Unknown => server.unwrap().to_string(),
+        Security:: Message => server.expect("ERROR: program stops").to_string(),
+        Security:: Warning => server.expect("WARNING: check the server").to_string(),
+        Security:: NotFound => server.expect("Not found: [{}]", server.ERROR).to_string(),
+        Security:: UnexpectedUrl => server.unwrap().to_string(),
     }
 }
