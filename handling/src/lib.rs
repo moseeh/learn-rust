574c031308cc3408901934275ea7654665a7511e
moonyango <mosesadrian825@gmail.com>
2025-04-09 14:59:42 +0300

    fix: format for readability

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -3,6 +3,10 @@ use std::io::Write;
 use std::path::Path;
 
 pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
-    let mut file = OpenOptions::new().create(true).append(true).open(path).unwrap();
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(path)
+        .unwrap();
     file.write_all(content.as_bytes()).unwrap();
 }
