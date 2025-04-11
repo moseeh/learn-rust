174444f399218a5796da6d53740806fdca170260
moonyango <mosesadrian825@gmail.com>
2025-04-11 16:42:00 +0300

    talking to the computer

---
+++ talking/src/lib.rs
@@ -0,0 +1,27 @@
+pub fn talking(text: &str) -> &str {
+    if text == "" {
+        return "Just say something!"
+    }
+    if s.trim_end().ends_with('?') {
+        if is_all_uppercase(text) {
+            return "Quiet, I am thinking!";
+        }
+        return "Sure."
+    } else {
+        if is_all_uppercase(text) {
+            return "There is no need to yell, calm down!"
+        }
+    }
+    "Interesting"
+}
+pub fn is_all_uppercase(s: &str) -> bool {
+    s.chars()
+        .filter(|c| c.is_ascii_alphabetic())
+        .all(|c| c.is_ascii_uppercase())
+}
+
+pub fn is_all_lowercase(s: &str) -> bool {
+    s.chars()
+        .filter(|c| c.is_ascii_alphabetic())
+        .all(|c| c.is_ascii_lowercase())
+}
