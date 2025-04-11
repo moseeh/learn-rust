a7dafa7b3d80055829f5977322bedbe968a15f51
moonyango <mosesadrian825@gmail.com>
2025-04-11 16:58:04 +0300

    modify uppercase function to avoid returning true when there is no letter

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -1,22 +1,22 @@
 pub fn talking(text: &str) -> &str {
     let trimmed = text.trim();
     if trimmed == "" {
-        return "Just say something!"
+        return "Just say something!";
     }
     if trimmed.ends_with('?') {
         if is_all_uppercase(trimmed) {
             return "Quiet, I am thinking!";
         }
-        return "Sure."
+        return "Sure.";
     } else {
         if is_all_uppercase(trimmed) {
-            return "There is no need to yell, calm down!"
+            return "There is no need to yell, calm down!";
         }
     }
     "Interesting"
 }
 pub fn is_all_uppercase(s: &str) -> bool {
-    s.chars()
-        .filter(|c| c.is_ascii_alphabetic())
-        .all(|c| c.is_ascii_uppercase())
+    let letters: Vec<char> = s.chars().filter(|c| c.is_ascii_alphabetic()).collect();
+
+    !letters.is_empty() && letters.iter().all(|c| c.is_ascii_uppercase())
 }
