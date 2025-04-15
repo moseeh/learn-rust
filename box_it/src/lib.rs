db4b6b0e26a815fe6360deff42fffd26058a0b97
moonyango <mosesadrian825@gmail.com>
2025-04-15 14:32:11 +0300

    box and unbox it

--- box_it/src/lib.rs
+++ box_it/src/lib.rs
@@ -2,7 +2,7 @@ pub fn transform_and_save_on_heap(s: String) -> Box<Vec<u32>> {
     Box::new(
         s.split_whitespace()
             .map(|s| {
-                if let Some(stripped) = s.strip_suffix('k') {
+                if let Some(stripped) = s.strip_suffix(|c| c == 'k' || c == 'K') {
                     stripped.parse::<u32>().expect("invalid number") * 1000
                 } else {
                     s.parse::<u32>().expect("Invalid number")
