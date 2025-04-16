6517b818636f3d9f3443147aaeda3bcf85c1c828
moonyango <mosesadrian825@gmail.com>
2025-04-16 17:39:41 +0300

    refactor

--- ref_cell/src/messenger.rs
+++ ref_cell/src/messenger.rs
@@ -1,55 +1,46 @@
 pub use std::rc::Rc;
 
-/// A logging trait that supports three different levels.
-pub trait Logger {
-    fn warning(&self, msg: &str);
-    fn info(&self, msg: &str);
-    fn error(&self, msg: &str);
-}
-
-/// Tracker keeps track of how many references to a particular value exist.
-/// It will use the logger to issue messages when the reference count exceeds
-/// various thresholds.
 pub struct Tracker<'a> {
     pub logger: &'a dyn Logger,
-    /// Last seen number of references.
-    pub value: usize,
-    /// Maximum allowed references.
-    pub max: usize,
+    pub value: Rc<i32>,
+    pub max: i32,
 }
 
+pub trait Logger {
+    fn info(&self, msg: &str);
+    fn error(&self, msg: &str);
+    fn warning(&self, msg: &str);
+}
+//&ref_cell::Worker
 impl<'a> Tracker<'a> {
-    /// Creates a new Tracker given a logger and a maximum allowed count.
-    pub fn new(logger: &'a dyn Logger, max: usize) -> Self {
+    //   pub  fn new(logger: &'a mut dyn Logger,num:i32) -> Tracker<'a> {
+    pub fn new(logger: &'a dyn Logger, num: i32) -> Tracker<'a> {
         Tracker {
             logger,
-            value: 0,
-            max,
+            value: Rc::new(num),
+            max: num,
         }
     }
-
-    /// Updates the current count of references (from the passed Rc) and
-    /// logs a message if the current reference count meets certain thresholds.
-    pub fn set_value(&mut self, tracked: &Rc<String>) {
-        let count = Rc::strong_count(tracked);
-        self.value = count;
-        let percentage = count * 100 / self.max;
-        if percentage >= 100 {
-            self.logger.error("you are over your quota!");
-        } else if percentage >= 70 {
-            let msg = format!(
-                "you have used up over {}% of your quota! Proceeds with precaution",
-                percentage
+    pub fn set_value(&self, num: &Rc<usize>) {
+        let count = Rc::strong_count(num);
+        let percent = (count * 100) / self.max as usize;
+        if percent >= 70 && percent < 100 {
+            self.logger.warning(
+                format!(
+                    "Warning: you have used up over {}% of your quota! Proceeds with precaution",
+                    percent
+                )
+                .as_str(),
             );
-            self.logger.warning(&msg);
+        }
+        if percent >= 100 {
+            self.logger.error("Error: you are over your quota!");
         }
     }
-
-    /// Peeks at the current reference count and logs an informational message.
-    pub fn peek(&self, tracked: &Rc<String>) {
-        let count = Rc::strong_count(tracked);
-        let percentage = count * 100 / self.max;
-        let msg = format!("you are using up to {}% of your quota", percentage);
-        self.logger.info(&msg);
+    pub fn peek(&self, num: &Rc<usize>) {
+        let count = Rc::strong_count(&num);
+        let percent = (count * 100) / self.max as usize;
+        self.logger
+            .info(format!("Info: you are using up to {}% of your quota", percent).as_str());
     }
 }
