be195fa12cdff793b77a026d48e57203ff65fe33
moonyango <mosesadrian825@gmail.com>
2025-04-18 00:27:30 +0300

    ref cell

--- ref_cell/src/lib.rs
+++ ref_cell/src/lib.rs
@@ -1,25 +1,190 @@
-pub use std::collections::HashMap;
-pub use std::cell::RefCell;
-pub use std::rc::Rc;
-
+// Import the messenger module and re-export its contents.
+mod messenger;
 pub use messenger::*;
 
-pub mod messenger;
+// Re-export HashMap from the standard collections module.
+pub use std::collections::HashMap;
 
-#[derive(Debug)]
+// Define a Worker struct to simulate a logger with reference tracking and message storage.
+#[derive(Clone, Debug)]
 pub struct Worker {
-    pub track_value: Rc<usize>,
-    pub mapped_messages: RefCell<HashMap<String, String>>,
-    pub all_messages: RefCell<Vec<String>>,
+    pub track_value: Rc<usize>, // Shared counter using reference counting.
+    pub mapped_messages: RefCell<HashMap<String, String>>, // Stores categorized messages (e.g., Warning -> message).
+    pub all_messages: RefCell<Vec<String>>, // Stores all messages in order they were logged.
 }
 
 impl Worker {
-pub    fn new(num:usize) -> Worker {
+    // Constructor method to initialize a new Worker instance with the given count.
+    pub fn new(s: usize) -> Worker {
         Worker {
-            track_value: Rc::new(num),
-            mapped_messages: RefCell::new(HashMap::new()),
-            all_messages: RefCell::new(Vec::new()),
+            track_value: Rc::new(s), // Create a new Rc with initial count.
+            mapped_messages: RefCell::new(HashMap::new()), // Initialize empty message map.
+            all_messages: RefCell::new(vec![]), // Initialize empty message list.
         }
     }
-  
+}
+
+// Implement the Logger trait for Worker to provide logging functionalities.
+impl Logger for Worker {
+    // Log a warning message.
+    fn warning(&self, message: &str) {
+        let v: Vec<&str> = message.split(": ").collect(); // Split message into category and content.
+        self.mapped_messages
+            .borrow_mut()
+            .insert(v[0].to_string(), v[1].to_string()); // Store message in map.
+        self.all_messages.borrow_mut().push(message.to_string()); // Append full message to list.
+    }
+
+    // Log an info message.
+    fn info(&self, message: &str) {
+        let v: Vec<&str> = message.split(": ").collect(); // Split message into category and content.
+        self.mapped_messages
+            .borrow_mut()
+            .insert(v[0].to_string(), v[1].to_string()); // Store message in map.
+        self.all_messages.borrow_mut().push(message.to_string()); // Append full message to list.
+    }
+
+    // Log an error message.
+    fn error(&self, message: &str) {
+        let v: Vec<&str> = message.split(": ").collect(); // Split message into category and content.
+        self.mapped_messages
+            .borrow_mut()
+            .insert(v[0].to_string(), v[1].to_string()); // Store message in map.
+        self.all_messages.borrow_mut().push(message.to_string()); // Append full message to list.
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_module() {
+        #[derive(Clone, Debug)]
+        struct TestMs {
+            value: Rc<usize>,
+            ms: RefCell<Vec<String>>,
+            correct: Vec<String>,
+        }
+
+        impl Logger for TestMs {
+            fn warning(&self, message: &str) {
+                self.ms.borrow_mut().push(message.to_string());
+            }
+            fn info(&self, message: &str) {
+                self.ms.borrow_mut().push(message.to_string());
+            }
+            fn error(&self, message: &str) {
+                self.ms.borrow_mut().push(message.to_string());
+            }
+        }
+
+        let l = TestMs {
+            value: Rc::new(115),
+            ms: RefCell::new(vec![]),
+            correct: vec![
+                String::from("Info: you are using up to 40% of your quota"),
+                String::from(
+                    "Warning: you have used up over 80% of your quota! Proceeds with precaution",
+                ),
+                String::from("Error: you are over your quota!"),
+            ],
+        };
+
+        let track = Tracker::new(&l, 5);
+        let _a = l.value.clone();
+        track.peek(&l.value); // 40%
+        let _a1 = l.value.clone();
+        let _a2 = l.value.clone();
+        track.set_value(&l.value); // 80%
+        let _a3 = l.value.clone();
+        track.set_value(&l.value); // 100%
+
+        for (i, v) in l.ms.into_inner().iter().enumerate() {
+            assert_eq!(v, &l.correct[i])
+        }
+    }
+
+    #[test]
+    fn test_module_usage_hasmap() {
+        let log = Worker::new(1000);
+        let track = Tracker::new(&log, 12);
+        let _clone_test = log.track_value.clone();
+        let _clone_test1 = log.track_value.clone();
+        let _clone_test2 = log.track_value.clone();
+        let _clone_test3 = log.track_value.clone();
+        let _clone_test4 = log.track_value.clone();
+        let _clone_test5 = log.track_value.clone();
+        let _clone_test6 = log.track_value.clone();
+        let _clone_test7 = log.track_value.clone();
+
+        // warning: 75% of the quota
+        track.set_value(&log.track_value);
+        assert_eq!(
+            log.mapped_messages.borrow().get("Warning").unwrap(),
+            "you have used up over 75% of your quota! Proceeds with precaution"
+        );
+
+        let _clone_test8 = log.track_value.clone();
+
+        // warning: 83% of the quota <- most resent of the messages last onw to be added to the hashmap
+        track.set_value(&log.track_value);
+        assert_eq!(
+            log.mapped_messages.borrow().get("Warning").unwrap(),
+            "you have used up over 83% of your quota! Proceeds with precaution"
+        );
+
+        // info: 83%
+        track.peek(&log.track_value);
+        assert_eq!(
+            log.mapped_messages.borrow().get("Info").unwrap(),
+            "you are using up to 83% of your quota"
+        );
+
+        let _clone_test9 = log.track_value.clone();
+        // info: 91%
+        track.peek(&log.track_value);
+        assert_eq!(
+            log.mapped_messages.borrow().get("Info").unwrap(),
+            "you are using up to 91% of your quota"
+        );
+
+        let _clone_test10 = log.track_value.clone();
+        // error: passed the quota
+        track.set_value(&log.track_value);
+        assert_eq!(
+            log.mapped_messages.borrow().get("Error").unwrap(),
+            "you are over your quota!"
+        );
+    }
+
+    #[test]
+    fn test_module_usage_vector() {
+        let correct = vec![
+            "Info: you are using up to 40% of your quota",
+            "Warning: you have used up over 80% of your quota! Proceeds with precaution",
+            "Info: you are using up to 80% of your quota",
+            "Error: you are over your quota!",
+        ];
+        let log = Worker::new(1);
+        let track = Tracker::new(&log, 5);
+        let _a = log.track_value.clone();
+        // info: 40%
+        track.peek(&log.track_value);
+        let _a1 = log.track_value.clone();
+        let _a2 = log.track_value.clone();
+
+        // warning: 80%
+        track.set_value(&log.track_value);
+        // info: 80%
+        track.peek(&log.track_value);
+        let _a3 = log.track_value.clone();
+
+        // error: passed the quota
+        track.set_value(&log.track_value);
+
+        for (i, v) in log.all_messages.into_inner().iter().enumerate() {
+            assert_eq!(v, correct[i]);
+        }
+    }
 }
--- ref_cell/src/messenger.rs
+++ ref_cell/src/messenger.rs
@@ -1,46 +1,64 @@
+// Re-export RefCell from std::cell to allow interior mutability.
+pub use std::cell::RefCell;
+// Re-export Rc from std::rc for reference-counted pointers.
 pub use std::rc::Rc;
 
-pub struct Tracker<'a> {
-    pub logger: &'a dyn Logger,
-    pub value: Rc<i32>,
-    pub max: i32,
+// Define a trait Logger with three logging methods.
+pub trait Logger {
+    fn warning(&self, msg: &str); // Method for warning messages.
+    fn info(&self, msg: &str);    // Method for info messages.
+    fn error(&self, msg: &str);   // Method for error messages.
 }
 
-pub trait Logger {
-    fn info(&self, msg: &str);
-    fn error(&self, msg: &str);
-    fn warning(&self, msg: &str);
+// Define a generic struct Tracker which logs usage of reference-counted values.
+#[derive(Debug, Clone)]
+pub struct Tracker<'a, T: Logger> {
+    logger: &'a T,               // Logger reference for emitting log messages.
+    value: RefCell<usize>,      // Interior mutable field to track current usage count.
+    max: usize,                 // Maximum usage limit for triggering warnings/errors.
 }
-//&ref_cell::Worker
-impl<'a> Tracker<'a> {
-    //   pub  fn new(logger: &'a mut dyn Logger,num:i32) -> Tracker<'a> {
-    pub fn new(logger: &'a dyn Logger, num: i32) -> Tracker<'a> {
+
+impl<'a, T> Tracker<'a, T>
+where
+    T: Logger, // Constraint: T must implement Logger trait.
+{
+    // Constructor method to create a new Tracker instance.
+    pub fn new(logger: &T, max: usize) -> Tracker<T> {
         Tracker {
-            logger,
-            value: Rc::new(num),
-            max: num,
+            logger,             // Store reference to logger.
+            value: RefCell::new(0), // Initialize internal counter to 0.
+            max,                // Set the max threshold.
         }
     }
-    pub fn set_value(&self, num: &Rc<usize>) {
-        let count = Rc::strong_count(num);
-        let percent = (count * 100) / self.max as usize;
-        if percent >= 70 && percent < 100 {
-            self.logger.warning(
-                format!(
-                    "Warning: you have used up over {}% of your quota! Proceeds with precaution",
-                    percent
-                )
-                .as_str(),
-            );
-        }
-        if percent >= 100 {
+
+    // Set the current value count based on the strong reference count of Rc<usize>.
+    pub fn set_value(&self, value: &Rc<usize>) {
+        self.value.replace(Rc::strong_count(value)); // Replace stored value with current reference count.
+
+        // Calculate how much of the quota has been used in percentage.
+        let percentage_of_max = convert_percentage(self.max, Rc::strong_count(&value));
+
+        // If usage is 100% or more, log an error.
+        if percentage_of_max >= 100 {
             self.logger.error("Error: you are over your quota!");
+            return;
+        // If usage is 70% or more, log a warning.
+        } else if percentage_of_max >= 70 {
+            self.logger
+                .warning(&format!("Warning: you have used up over {percentage_of_max}% of your quota! Proceeds with precaution"));
         }
     }
-    pub fn peek(&self, num: &Rc<usize>) {
-        let count = Rc::strong_count(&num);
-        let percent = (count * 100) / self.max as usize;
-        self.logger
-            .info(format!("Info: you are using up to {}% of your quota", percent).as_str());
+
+    // Peek current usage and log it as informational message.
+    pub fn peek(&self, value: &Rc<usize>) {
+        let percentage_of_max = convert_percentage(self.max, Rc::strong_count(&value)); // Calculate current usage.
+        self.logger.info(&format!(
+            "Info: you are using up to {percentage_of_max}% of your quota" // Log info with current usage percentage.
+        ))
     }
+}
+
+// Helper function to convert current value to percentage of max.
+fn convert_percentage(max: usize, v: usize) -> usize {
+    (100 * v) / max // Return v as a percentage of max.
 }
