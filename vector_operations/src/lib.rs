b9615c7f72910ac57722bf3430c522ba42a1de20
moonyango <mosesadrian825@gmail.com>
2025-04-19 23:20:12 +0300

    feat: addition and subtraction for 3d vectors

---
+++ vector_operations/src/lib.rs
@@ -0,0 +1,31 @@
+use std::ops::{Add, Sub};
+
+pub struct ThreeDVector<T> {
+    pub i: T,
+    pub j: T,
+    pub k: T,
+}
+
+impl<T: Add<Output = T>> Add for ThreeDVector<T> {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        Self {
+            i: self.i + other.i,
+            j: self.j + other.j,
+            k: self.k + other.k,
+        }
+    }
+}
+
+impl<T: Sub<Output = T>> Sub for ThreeDVector<T> {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        Self {
+            i: self.i - other.i,
+            j: self.j - other.j,
+            k: self.k - other.k,
+        }
+    }
+}
