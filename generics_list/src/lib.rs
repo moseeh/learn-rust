112eac94dbdf7d55b243e47014a380ef81800311
moonyango <mosesadrian825@gmail.com>
2025-04-19 22:06:43 +0300

    singly linked list

--- generics_list/src/lib.rs
+++ generics_list/src/lib.rs
@@ -1,3 +1,4 @@
+/// A singly linked list of generic values.
 #[derive(Clone, Debug)]
 pub struct List<T> {
     pub head: Option<Node<T>>,
@@ -10,49 +11,35 @@ pub struct Node<T> {
 }
 
 impl<T> List<T> {
-    pub fn new() -> List<T> {
+    /// Create a new empty list.
+    pub fn new() -> Self {
         List { head: None }
     }
 
+    /// Push a value onto the front of the list.
     pub fn push(&mut self, value: T) {
-        let new_node = Node { value, next: None };
-
-        match self.head.take() {
-            None => {
-                // If the list is empty, set the new node as the head
-                self.head = Some(new_node);
-            }
-            Some(old_head) => {
-                // Create a new node with the value and the old head as next
-                let mut new_head = new_node;
-                new_head.next = Some(Box::new(old_head));
-                self.head = Some(new_head);
-            }
-        }
+        let new_node = Node {
+            value,
+            next: self.head.take().map(Box::new),
+        };
+        self.head = Some(new_node);
     }
 
+    /// Pop (remove) the first element of the list.
     pub fn pop(&mut self) {
-        if let Some(head) = self.head.take() {
-            // Set the head to be the next node
-            self.head = match head.next {
-                Some(next_node) => Some(*next_node),
-                None => None,
-            };
+        if let Some(node) = self.head.take() {
+            self.head = node.next.map(|boxed| *boxed);
         }
     }
 
+    /// Return the number of elements in the list.
     pub fn len(&self) -> usize {
         let mut count = 0;
-        let mut current = &self.head;
-
+        let mut current = self.head.as_ref();
         while let Some(node) = current {
             count += 1;
-            current = match &node.next {
-                Some(boxed_node) => &Some(**boxed_node),
-                None => &None,
-            };
+            current = node.next.as_deref();
         }
-
         count
     }
 }
