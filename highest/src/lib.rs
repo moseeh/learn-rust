dc838f1cdfc25a5e716376ae29495aa6f8eb37fd
moonyango <mosesadrian825@gmail.com>
2025-04-26 02:26:42 +0300

    highest

---
+++ highest/src/lib.rs
@@ -0,0 +1,29 @@
+#[derive(Debug)]
+pub struct Numbers<'a> {
+    numbers: &'a [u32],
+}
+
+impl<'a> Numbers<'a> {
+    pub fn new(numbers: &[u32]) -> Self {
+        Numbers { numbers }
+    }
+
+    pub fn list(&self) -> &[u32] {
+        self.numbers
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.numbers.last().copied()
+    }
+
+    pub fn highest(&self) -> Option<u32> {
+        self.numbers.iter().max().copied()
+    }
+
+    pub fn highest_three(&self) -> Vec<u32> {
+        let mut nums = self.numbers.to_vec();
+        nums.sort_unstable_by(|a, b| b.cmp(a)); // descending
+        nums.truncate(3);
+        nums
+    }
+}
