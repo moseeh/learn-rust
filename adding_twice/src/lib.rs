78bb022c311995a51ca74c90d240b14cd98806ef
moonyango <mosesadrian825@gmail.com>
2025-04-26 02:10:23 +0300

    add curry twice

--- adding_twice/src/lib.rs
+++ adding_twice/src/lib.rs
@@ -4,7 +4,7 @@ pub fn add_curry(x: i32) -> impl Fn(i32) -> i32 {
 
 pub fn twice<F>(f: F) -> impl Fn(i32) -> i32
 where
-    F: Fn(i32) -> i32 + Copy,
+    F: Fn(i32) -> i32, 
 {
     move |x| f(f(x))
 }
